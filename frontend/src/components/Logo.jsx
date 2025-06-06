e2b6fc79a6be9ea2bc14cc461b60271daab8970a
kacper32332 <kacperrogowski1cpp@gmail.com>
2025-06-06 12:42:55 +0200

    Working on Home.jsx - searchbar implementation

---
+++ frontend/src/components/Logo.jsx
@@ -0,0 +1,7 @@
+function Logo() {
+	return (
+		<div className="p-20 flex flex-wrap justify-center">miejsce na logo</div>
+	)
+}
+
+export default Logo
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,17 +1,19 @@
-// import WelcomeWindow from "../components/WelcomeWindow";
+import Logo from "../components/Logo";
+import Searchbar from "../components/Searchbar";
 
 function Home() {
 	return (
 		<div>
-			{/* <WelcomeWindow /> */}
-			<div className="flex flex-col items-center justify-center min-h-screen bg-gray-700">
+			<Logo />
+			<Searchbar />
+			{/* <div className="flex flex-col items-center justify-center min-h-screen bg-gray-700">
 				<h1 className="text-4xl font-bold mb-4 text-gray-400">
 					Welcome to the Home Page
 				</h1>
 				<p className="text-lg text-gray-400">
 					This is the home page of your application.
 				</p>
-			</div>
+			</div> */}
 		</div>
 	);
 }
